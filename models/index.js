9781489adf002e17f9471fc2352925a9be219e2b
Tuan Le <tuanleminh1235@gmail.com>
2019-01-31 22:03:07 -0500

    Update DB connect

--- models/index.js
+++ models/index.js
@@ -1,14 +1,36 @@
-const { mongoose } = require("../singleton")
+const mongoose = require("mongoose")
 
-mongoose.set("debug", true)
-mongoose.Promise = Promise
-mongoose.connect(process.env.MONGODB, {
-    keepAlive: true,
-    useNewUrlParser: true
-})
+const options = {
+    useFindAndModify: false,
+    autoIndex: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    poolSize: 500,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
+}
+
+const attachEventListeners = () => {
+    mongoose.connection.on("connected", () => {
+        console.log("MongoDB connected")
+    })
+    mongoose.connection.on("error", error => {
+        console.error(`MongoDB connection error: ${error}`)
+    })
+    mongoose.connection.on("disconnected", () => {
+        console.log("MongoDB disconnected")
+    })
+}
+
+const connect = () => {
+    mongoose.set("debug", true)
+    mongoose.Promise = global.Promise
+    mongoose
+        .connect(process.env.MONGODB_URL, options)
+        .then(attachEventListeners)
+        .catch(error => console.error(`MongoDB connection error: ${error}`))
+}
 
 module.exports = {
-    User: require("./User"),
-    Message: require("./Message"),
-    Room: require("./Room")
+    connect
 }
