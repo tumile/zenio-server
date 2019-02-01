d0929b90088400ba3446f9eecf1bf8f0040fe21c
Tuan Le <tuanleminh1235@gmail.com>
2019-01-31 22:26:03 -0500

    Get messages and pagination

--- handlers/RoomHandler.js
+++ handlers/RoomHandler.js
@@ -3,20 +3,50 @@ const User = require("../models/User")
 
 exports.getAllRooms = async (req, res, next) => {
     try {
-        const user = await User.findById(req.params.userId)
-        const rooms = await Room.find({ _id: { $in: user.rooms } })
+        const user = await db.User.findById(req.params.userId)
+        const rooms = await db.Room.find({ _id: { $in: user.rooms } })
             .sort({ updatedAt: -1 })
             .skip(parseInt(req.query.skip))
-            // .limit() // TODO: Pagination
+            .limit(PAGE_LIMIT)
             .populate({
                 path: "members",
                 select: "firstName lastName avatar"
             })
             .populate({
-                path: "messages"
+                path: "messages",
+                options: {
+                    limit: PAGE_LIMIT,
+                    sort: { createdAt: -1 }
+                }
             })
         res.status(200).json({
-            rooms
+            rooms,
+            canLoadMore: !(rooms.length < PAGE_LIMIT)
+        })
+    } catch (error) {
+        next({
+            status: 500,
+            type: "DATABASE_ERROR"
+        })
+    }
+}
+
+exports.getMessagesInRoom = async (req, res, next) => {
+    try {
+        const room = await db.Room.findById(
+            req.params.roomId,
+            "messages"
+        ).populate({
+            path: "messages",
+            options: {
+                sort: { createdAt: -1 },
+                skip: parseInt(req.query.skip),
+                limit: PAGE_LIMIT
+            }
+        })
+        res.status(200).json({
+            messages: room.messages,
+            canLoadMore: !(room.messages < PAGE_LIMIT)
         })
     } catch (error) {
         next({
